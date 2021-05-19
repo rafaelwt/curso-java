6fcc85ae908c06a5d36bbfce93fffdc69d86c51b
rafael <rafael.wt@gmail.com>
2021-05-18 21:11:47 -0400

    primer commit

---
+++ src/ExampleCalendar.java
@@ -0,0 +1,18 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ExampleCalendar {
+
+    public static void main (String [] args) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2020, Calendar.SEPTEMBER, 25, 18, 20, 10);
+        Date date = calendar.getTime();
+
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        String fechaFormato = formato.format(date);
+        System.out.println("date = " + date);
+        System.out.println("fechaFormato = " + fechaFormato);
+    }
+}
